693a5adc9a6b2ccef2394c64dc19642cf08afbaa
Ailani M Borges Lopez <aborgesl@charlotte.edu>
2026-02-17 23:02:07 -0500

    Major changes: Making login.html into login.ejs, adding styling to restaurants.ejs via restaurants-style.css, and editing the styling for login.ejs

--- hello.js
+++ hello.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -14,7 +16,7 @@ const restaurantRoutes = require("./routes/restaurant");
 app.use("/restaurants", restaurantRoutes);
 
 app.get("/", (req, res) => {
-  res.send("CharlotteEats server is running!");
+  res.render("login");
 });
 
 const PORT = 8000;
