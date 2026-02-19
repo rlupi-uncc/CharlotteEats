b836dba535b4ac92b68b65b461cc22712a28746c
Bryson Burrell <bburrel4@charlotte.edu>
2026-02-19 18:51:52 -0500

    Update authRoutes.js
    Updated /signup route to /register for consistency with frontend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import { signUpHandler, logInHandler } from '../controllers/authController.js';
 import { validateUser } from '../middleware/userValidators.js';
 const router = express.Router();
 
-router.post('/signup', validateUser, signUpHandler);
+router.post('/register', validateUser, signUpHandler);
 router.post('/login', validateUser, logInHandler);
 
 export default router;
