ca8de99b27178c1198628a765933d5f494bba00e
cmeado11 <cmeado11@charlotte.edu>
2026-02-22 19:44:46 -0500

    Added review middleware and attached middleware to reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router( {mergeParams: true} );
 
 const reviewController = require('../controllers/reviewController.js');
+const {
+    validateRestaurantId,
+    validateReviewId,
+    validateCreateReview,
+    validateUpdateReview,
+} = require('../middleware/reviewValidators.js');
 
-router.post('/', reviewController.createReviewHandler);
+router.use(validateRestaurantId);
+
+router.post('/', validateCreateReview, reviewController.createReviewHandler);
 router.get('/', reviewController.getReviewsHandler);
-router.get('/:reviewId', reviewController.getReviewByIdHandler);
-router.put('/:reviewId', reviewController.updateReviewHandler);
-router.delete('/:reviewId', reviewController.deleteReviewHandler);
+router.get('/:reviewId', validateReviewId, reviewController.getReviewByIdHandler);
+router.put('/:reviewId', validateReviewId, validateUpdateReview, reviewController.updateReviewHandler);
+router.delete('/:reviewId', validateReviewId, reviewController.deleteReviewHandler);
 
 module.exports = router;
