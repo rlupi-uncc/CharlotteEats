db70233f14d195f6abbb263dd476d30628e32b1f
Bryson Burrell <bburrel4@charlotte.edu>
2026-03-17 15:09:17 -0400

    Logout Functionality
    Adds a route to authRoutes and controller function to authController to allow the user to log out.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/register", validateUser, authController.signUpHandler);
 router.post("/login", validateLogin, authController.logInHandler);
+router.post("/logout", authController.logOutHandler);
 
 module.exports = router;
